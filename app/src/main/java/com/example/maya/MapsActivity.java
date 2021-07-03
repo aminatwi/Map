cae96ebf321d5a5c80616ac9d440476e34ea254d
heba <iheba34@gmail.com>
2021-07-03 17:24:39 +0300

    last commit

--- app/src/main/java/com/example/maya/MapsActivity.java
+++ app/src/main/java/com/example/maya/MapsActivity.java
@@ -102,16 +102,118 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         return nearest;
     }
     private void insertInformation() {
+        Restaurant restaurant=new Restaurant();
+        //restaurant.setId(1);
+        restaurant.setV(33.4511800);
+        restaurant.setV1(35.48880000);
+        restaurant.setName("McDonalds");
+        restaurant.setDescription("Good food");
+        dbHandler.AddRestautrant(restaurant);
+
+        Restaurant restaurant2=new Restaurant();
+        //restaurant2.setId(2);
+        restaurant2.setV(33.9796587);
+        restaurant2.setV1(35.6509818);
+        restaurant2.setName("Pizza Hut");
+        restaurant2.setDescription("Good food");
+        dbHandler.AddRestautrant(restaurant2);
+
+        Restaurant restaurant3=new Restaurant();
+        //restaurant3.setId(3);
+        restaurant3.setV(33.9796587);
+        restaurant3.setV1(35.6509818);
+        restaurant3.setName("Burger King");
+        restaurant3.setDescription("Good food");
+        dbHandler.AddRestautrant(restaurant3);
+
+        Hotel hotel=new Hotel();
+        //hotel.setId(1);
+        hotel.setV(33.9795586);
+        hotel.setV1(35.6518918);
+        hotel.setName("Harissa Hotel");
+        hotel.setDescription("5 stars hotel");
+        dbHandler.AddHotel(hotel);
+
+        Hotel hotel1=new Hotel();
+        //hotel.setId(2);
+        hotel1.setV(33.9795585);
+        hotel1.setV1(35.6518933);
+        hotel1.setName("Grand Hotel");
+        hotel1.setDescription("4 stars hotel");
+        dbHandler.AddHotel(hotel1);
+
         InfoWindowData info = new InfoWindowData();
+        //info.setId(1);
         info.setV(33.4510800);
         info.setV1(35.48870000);
         info.setTitle("Maya Home");
         info.setSnippet("Maya Home located in Ain Qana.");
-        info.setHotel("Hotel : excellent hotels available");
-        info.setFood("Food : all types of restaurants available");
+        String h="Hotels near:\n";
+        for(int i=0;i<nearHotels(info.getV(),info.getV1()).size();i++){
+            h+="\n"+nearHotels(info.getV(),info.getV1()).get(i).getName();
+        }
+        if(nearHotels(info.getV(),info.getV1()).size()==0) info.setHotel("No Hotels near");
+        else info.setHotel(h);
+
+        String r="Restaurants near:\n";
+        for(int i=0;i<nearRestaurants(info.getV(),info.getV1()).size();i++){
+            r+="\n"+nearRestaurants(info.getV(),info.getV1()).get(i).getName();
+        }
+        if(nearRestaurants(info.getV(),info.getV1()).size()==0) info.setFood("No near restaurants");
+        else info.setFood(r);
+
         info.setTransport("Reach the site by bus, car and train.");
         info.setImage("png/Maya.png");
+        //info.setAudio_number(1);
         dbHandler.AddInfoWindowData(info);
+
+        InfoWindowData info2 = new InfoWindowData();
+        //info2.setId(2);
+        info2.setV(33.9795587);
+        info2.setV1(35.6508818);
+        info2.setTitle("Harissa");
+        info2.setSnippet("Lady of Lebanon");
+        String hh="Hotels near:\n";
+        for(int i=0;i<nearHotels(info2.getV(),info2.getV1()).size();i++){
+            hh+="\n"+nearHotels(info2.getV(),info2.getV1()).get(i).getName();
+        }
+        if(nearHotels(info2.getV(),info2.getV1()).size()==0) info2.setHotel("No near hotels");
+        else info2.setHotel(hh);
+        String rr="Restaurants near:\n";
+        for(int i=0;i<nearRestaurants(info2.getV(),info2.getV1()).size();i++){
+            rr+="\n"+nearRestaurants(info2.getV(),info2.getV1()).get(i).getName();
+        }
+        if(nearRestaurants(info2.getV(),info2.getV1()).size()==0) info2.setFood("No near restaurants");
+        else info2.setFood(rr);
+        info2.setTransport("Reach the site by bus, car and train.");
+        info2.setImage("png/Harissa.png");
+        //info2.setAudio_number(1);
+        dbHandler.AddInfoWindowData(info2);
+        for(int i=0;i<2;i++){
+            Image def=new Image();
+            def.setId_place(dbHandler.getAllInfoWindowData().get(i).getId());
+            def.setPic(dbHandler.getAllInfoWindowData().get(i).getImage());
+            dbHandler.AddImage(def);
+        }
+
+
+        Image img1=new Image();
+        img1.setId_place(2);
+        img1.setPic("png/telefrique.png");
+
+        Image img2=new Image();
+        img2.setId_place(2);
+        img2.setPic("png/church.png");
+
+        Image img3=new Image();
+        img3.setId_place(2);
+        img3.setPic("png/view.png");
+
+        dbHandler.AddImage(img3);
+        dbHandler.AddImage(img2);
+        dbHandler.AddImage(img1);
+
+
     }
 
 
